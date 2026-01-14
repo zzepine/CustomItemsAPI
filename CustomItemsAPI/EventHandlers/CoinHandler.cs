6e9c0534abd1d58babe7374f9ef20c847a045b22
Zepine <55946112+zzepine@users.noreply.github.com>
2026-01-14 19:23:55 +0100

    added CustomCoinBase

---
+++ CustomItemsAPI/EventHandlers/CoinHandler.cs
@@ -0,0 +1,29 @@
+﻿using CustomItemsAPI.Events;
+using CustomItemsAPI.Items;
+using LabApi.Events.Arguments.PlayerEvents;
+using LabApi.Events.CustomHandlers;
+
+namespace CustomItemsAPI.EventHandlers;
+
+internal sealed class CoinHandler : CustomEventsHandler
+{
+    public override void OnPlayerFlippingCoin(PlayerFlippingCoinEventArgs ev)
+    {
+        if (!CustomItems.TryGetCustomItem(ev.CoinItem, out CustomCoinBase cur_item))
+            return;
+        TypeWrapper<bool> isTailsHelper = new(ev.IsTails);
+        TypeWrapper<bool> isAllowedHelper = new(ev.IsAllowed);
+        CustomCoinEvents.OnFlipping(cur_item, ev.Player, ev.CoinItem, isTailsHelper, isAllowedHelper);
+        cur_item.OnFlipping(ev.Player, ev.CoinItem, isTailsHelper, isAllowedHelper);
+        ev.IsTails = isTailsHelper.Value;
+        ev.IsAllowed = isAllowedHelper.Value;
+    }
+
+    public override void OnPlayerFlippedCoin(PlayerFlippedCoinEventArgs ev)
+    {
+        if (!CustomItems.TryGetCustomItem(ev.CoinItem, out CustomCoinBase cur_item))
+            return;
+        CustomCoinEvents.OnFlipped(cur_item, ev.Player, ev.CoinItem, ev.IsTails);
+        cur_item.OnFlipped(ev.Player, ev.CoinItem, ev.IsTails);
+    }
+}
---
+++ CustomItemsAPI/Events/CustomCoinEvents.cs
@@ -0,0 +1,19 @@
+﻿using CustomItemsAPI.Items;
+using LabApi.Features.Wrappers;
+
+namespace CustomItemsAPI.Events;
+
+/// <summary>
+/// Events for calling methods for <see cref="CustomCoinBase"/>.
+/// </summary>
+public static class CustomCoinEvents
+{
+    public static event Action<CustomCoinBase, Player, CoinItem, TypeWrapper<bool>, TypeWrapper<bool>>? Flipping;
+    public static event Action<CustomCoinBase, Player, CoinItem, bool>? Flipped;
+
+    public static void OnFlipping(CustomCoinBase customItem, Player player, CoinItem coin, TypeWrapper<bool> isTails, TypeWrapper<bool> isAllowedHelper)
+        => Flipping?.Invoke(customItem, player, coin, isTails, isAllowedHelper);
+
+    public static void OnFlipped(CustomCoinBase customItem, Player player, CoinItem coin, bool isTails)
+        => Flipped?.Invoke(customItem, player, coin, isTails);
+}
